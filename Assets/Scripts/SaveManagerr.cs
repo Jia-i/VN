e5dd048e2dfae8aca14366a894e27c54d430e7e5
Xin Yi <103635699+Xinyi0917@users.noreply.github.com>
2026-09-15 21:14:01 +0800

    Test
    存档load档

--- Assets/Scripts/SaveManagerr.cs
+++ Assets/Scripts/SaveManagerr.cs
@@ -5,16 +5,73 @@ public class SaveManager : MonoBehaviour
 {
     public DialogueRunner dialogueRunner;
 
-    public void OnSaveButtonClicked()
+    public GameObject savePanel;
+    public GameObject loadPanel;
+
+    public void OpenSavePanel()
     {
-        dialogueRunner.SaveStateToPersistentStorage("save1");
+        savePanel.SetActive(true);
     }
 
-    public void OnLoadButtonClicked()
+    public void CloseSavePanel()
     {
-    dialogueRunner.LoadStateFromPersistentStorage("save1");
+        savePanel.SetActive(false);
+    }
 
-    //string nodeName = dialogueRunner.VariableStorage.GetValue("$current_node").ToString();
-    //dialogueRunner.StartDialogue(nodeName);
+    public void OpenLoadPanel()
+    {
+        loadPanel.SetActive(true);
+    }
+
+    public void CloseLoadPanel()
+    {
+        loadPanel.SetActive(false);
+    }
+
+    public void SaveSlot1()
+    {
+        SaveGame("Save1");
+    }
+
+    public void SaveSlot2()
+    {
+        SaveGame("Save2");
+    }
+
+    private void SaveGame(string slotName)
+    {
+        string currentNode = dialogueRunner.Dialogue.CurrentNode;
+
+        PlayerPrefs.SetString(slotName + "_Node", currentNode);
+
+        dialogueRunner.SaveStateToPersistentStorage(slotName);
+
+        PlayerPrefs.Save();
+
+        Debug.Log("Game Saved: " + slotName + " / " + currentNode);
+    }
+
+    public void LoadSlot1()
+    {
+        LoadGame("Save1");
+    }
+
+    public void LoadSlot2()
+    {
+        LoadGame("Save2");
+    }
+
+    private void LoadGame(string slotName)
+    {
+        dialogueRunner.LoadStateFromPersistentStorage(slotName);
+
+        string currentNode = PlayerPrefs.GetString(
+            slotName + "_Node",
+            "Start"
+        );
+
+        dialogueRunner.StartDialogue(currentNode);
+
+        Debug.Log("Game Loaded: " + slotName + " / " + currentNode);
     }
 }
